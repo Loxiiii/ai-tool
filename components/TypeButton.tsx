b64430e3fb3a3435c71c72976cd02032c60a13d1
Javi <javiercamposcastello@gmail.com>
2023-01-22 20:45:04 -0600

    Added ChatGPT Functionality on the /hello route

--- components/TypeButton.tsx
+++ components/TypeButton.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 interface TypeButtonProps {
   text: string
 }
 
+const testRequest = () => {
+  console.log('You just clicked');
+  axios({
+    method: 'post',
+    url: '/api/hello',
+    data: {
+      prompt: 'Hello, nice to meet you! what is your name?',
+    },
+  });
+};
+
 function TypeButton({ text }: TypeButtonProps) {
   return (
-    <div className="bg-purple-400 hover:bg-purple-500 active:bg-purple-600 focus:outline-none focus:ring focus:ring-violet-300 text-white font-bold py-2 px-4 rounded cursor-pointer">{text}</div>
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div
+      className="bg-purple-400 hover:bg-purple-500 active:bg-purple-600 focus:outline-none focus:ring focus:ring-violet-300 text-white font-bold py-2 px-4 rounded cursor-pointer"
+      onClick={() => {
+        testRequest();
+      }}
+    >
+      {text}
+
+    </div>
   );
 }
 
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
+import axios from 'axios';
 
-type Data = {
-  name: string
-}
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+const configuration = new Configuration({
+  organization: 'org-WdNnqXnrMVHyFaj1GxKYOiQL',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>,
+// ) {
+//   // console.log('You hit the /hello route');
+//   res.status(200).json({ name: 'John Doe' });
+// }
+
+export default function handleTest(req, res) {
+  console.log(req.body.prompt);
+  openai.createCompletion({
+    model: 'text-davinci-003',
+    // prompt: req.body.prompt,
+    prompt: 'please type a short cover letter for Google',
+    max_tokens: 400,
+    temperature: 0,
+  }).then((data) => {
+    console.log(data.data.choices);
+  });
+  res.end();
 }
